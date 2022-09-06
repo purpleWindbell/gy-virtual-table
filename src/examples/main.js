f10472530703afd9dd85f463ce39c590980b1afc
purpleWindbell <13225920965@139.com>
2022-09-06 17:34:02 +0800

    文件位置调整

---
+++ src/examples/main.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @StartVersion: 2.0
+ * @LastVersion: 2.0
+ * @Autor: gaoyue
+ * @Date: 2022-09-06 17:05:41
+ * @LastEditTime: 2022-09-06 17:19:26
+ */
+import Vue from 'vue'
+import App from './index.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,40 @@
+/*
+ * @Description: 
+ * @StartVersion: 2.0
+ * @LastVersion: 2.0
+ * @Autor: gaoyue
+ * @Date: 2022-09-06 17:10:36
+ * @LastEditTime: 2022-09-06 17:23:06
+ */
+const path = require('path')
+module.exports = {
+  // 修改 src 为 examples
+  pages: {
+    index: {
+      entry: "src/examples/main.js",
+      template: "public/index.html",
+      filename: "index.html"
+    }
+  },
+  // 扩展 webpack 配置，使 packages 加入编译
+  chainWebpack: config => {
+     // @ 默认指向 src 目录，这里要改成 examples
+    // 另外也可以新增一个 ~ 指向 packages
+    config.resolve.alias
+      .set('@', path.resolve('src'))
+      .set('~', path.resolve('package'))
+
+    config.module
+      .rule('js')
+      .include
+        .add(/package/)
+        .end().include.add(/examples/)
+        .end()
+      .use('babel')
+        .loader('babel-loader')
+        .tap(options => {
+          // 修改它的选项...
+          return options
+        })
+  }
+};
